a500128454f5ac7a7dd4711cc0acf627ee1711d4
Haris <Haris@DESKTOP-CPJCV58>
2020-11-30 16:16:32 +0100

    Add .gitignore and .gitattributes.

---
+++ leave-managment/Repository/LeaveTypeRepository.cs
@@ -0,0 +1,64 @@
+﻿using leave_managment.Contracts;
+using leave_managment.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_managment.Repository
+{
+    public class LeaveTypeRepository : ILeaveTypeRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public LeaveTypeRepository(ApplicationDbContext db)
+        {
+            _db = db; 
+        }
+
+        public bool Create(LeaveType entity)
+        {
+            _db.LeaveTypes.Add(entity);
+            // Save
+            return Save();
+        }
+
+        public bool Delete(LeaveType entity)
+        {
+            _db.LeaveTypes.Remove(entity);
+            // Save
+            return Save();
+        }
+
+        public ICollection<LeaveType> FindAll()
+        {
+            var leaveTypes = _db.LeaveTypes.ToList();
+            return leaveTypes;
+        }
+
+        public LeaveType FindById(int id)
+        {
+            var leaveType = _db.LeaveTypes.Find(id);
+            return leaveType;
+        }
+
+        public ICollection<LeaveType> GetEmployeesByLeaveType(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save()
+        {
+            var changes = _db.SaveChanges();
+            return changes > 0;
+            //return _db.SaveChanges() > 0;
+        }
+
+        public bool Update(LeaveType entity)
+        {
+            _db.LeaveTypes.Update(entity);
+            // Save
+            return Save();
+        }
+    }
+}
